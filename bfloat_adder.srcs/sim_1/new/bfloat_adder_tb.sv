fc446a8e66b2fb85b4aa94c6965f538ae8c57732
DineshVenkatG <99543009+DineshVenkatG@users.noreply.github.com>
2023-06-06 11:33:05 +0530

    Add files via upload

---
+++ bfloat_adder.srcs/sim_1/new/bfloat_adder_tb.sv
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 31.03.2023 12:32:52
+// Design Name: 
+// Module Name: bfloat_adder_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+module bfloat_adder_tb();
+reg [15:0]a,b;
+wire [15:0]c;
+
+bfloat_adder DUT(a,b,c);
+initial begin
+a=16'b0100001000011000;
+b=16'b0100000101010000;
+#250 $finish;
+end
+endmodule
+
+
